79417b0ad30a30beb31bbf5489f1b7b33b876915
Sarif Hidayatullah <hi.sarifhidayatullah@gmail.com>
2024-12-05 22:19:23 +0700

    Update codecept

--- codecept.conf.js
+++ codecept.conf.js
@@ -1,5 +1,5 @@
 /* eslint-disable import/no-extraneous-dependencies */
-const { setHeadlessWhen, setCommonPlugins } = require('@codeceptjs/configure');
+const { setHeadlessWhen, setCommonPlugins } = require("@codeceptjs/configure");
 // turn on headless mode when running with HEADLESS=true environment variable
 // export HEADLESS=true && npx codeceptjs run
 setHeadlessWhen(process.env.HEADLESS);
@@ -9,16 +9,17 @@ setCommonPlugins();
 
 /** @type {CodeceptJS.MainConfig} */
 exports.config = {
-  tests: 'e2e/**/*.spec.js',
-  output: 'e2e/outputs',
+  tests: "e2e/**/*.spec.js",
+  output: "e2e/outputs",
   helpers: {
     Playwright: {
-      url: 'http://127.0.0.1:4444',
+      browser: "chromium",
+      url: "http://localhost:8080", // Update this to match your development server URL
       show: true,
     },
   },
   include: {
-    I: './steps_file.js',
+    I: "./steps_file.js",
   },
-  name: 'Submission Expert 2 Restaurant',
+  name: "Submission Expert 2 Restaurant",
 };
