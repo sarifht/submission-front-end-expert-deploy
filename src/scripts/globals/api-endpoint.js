3751e5b9edaa1928bfed012f03f6e96aa7a72d08
Sarif Hidayatullah <hi.sarifhidayatullah@gmail.com>
2024-12-05 21:59:11 +0700

    Update config

--- src/scripts/globals/api-endpoint.js
+++ src/scripts/globals/api-endpoint.js
@@ -1,9 +1,10 @@
-// Sarif Hidayatullah
-import CONFIG from './config';
+import CONFIG from "./config";
 
 const API_ENDPOINT = {
-  HOME: `${CONFIG.BASE_URL}list`,
-  DETAIL: (id) => `${CONFIG.BASE_URL}detail/${id}`,
+  LIST: `${CONFIG.BASE_URL}/list`,
+  DETAIL: (id) => `${CONFIG.BASE_URL}/detail/${id}`,
+  // SEARCH: (query) => `${CONFIG.BASE_URL}/search?q=${query}`,
+  ADD_REVIEW: `${CONFIG.BASE_URL}/review`,
 };
 
 export default API_ENDPOINT;
